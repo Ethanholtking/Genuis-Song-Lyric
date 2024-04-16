bbc9368455dbc92d1708fb1bd1441e2ea2892b4e
Ethanholtking <127417494+Ethanholtking@users.noreply.github.com>
2024-04-16 12:40:59 -0400

    feat: Added comments and red black tree class

--- src/red_black.h
+++ src/red_black.h
@@ -3,15 +3,42 @@ using namespace std;
 
 class red_black_node 
 {
+	// Variables for the nide
 	bool red;
 	string artist;
 	string title;
+	red_black_node* left;
+	red_black_node* right;
 	
 public:
+	// Constructor
 	red_black_node(string artist, string title) 
 	{
 		this->title = title;
 		this->artist = artist;
+		left = nullptr;
+		reight = nullptr;
 		red = true;
+	}
+};
+
+class red_black_tree 
+{
+	red_black_node* root;
+public: 
+	//Constructor
+	red_black_tree() 
+	{
+		root = nullptr;
+	}
+	// Getter
+	red_black_node* get_root() 
+	{
+		return this->root;
+	}
+	// Setter
+	void set_root(red_black_node* root) 
+	{
+		this->root = root;
 	}
 };
