461b4fd551f75ffdb104138120428bbeb168d8ba
Ethanholtking <127417494+Ethanholtking@users.noreply.github.com>
2024-04-16 12:06:26 -0400

    Added the red black tree node and constructor

--- src/red_black.h
+++ src/red_black.h
@@ -1,1 +1,17 @@
+#include <string>
+using namespace std;
 
+class red_black_node 
+{
+	bool red;
+	string artist;
+	string title;
+	
+public:
+	red_black_node(string artist, string title) 
+	{
+		this->title = title;
+		this->artist = artist;
+		red = true;
+	}
+};
