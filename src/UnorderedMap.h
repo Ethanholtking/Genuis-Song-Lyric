dc2e258eacb4f837e40c4f8b34b607bf46fdac5c
Evan <evanmstice@comcast.net>
2024-04-16 19:49:12 -0400

    Implemented initial functionality for Unordered Map Hash Table - had to copy from another CLion project because CMake compiling wasn't working on previous project (thus history/commits aren't shown)

---
+++ src/UnorderedMap.h
@@ -0,0 +1,32 @@
+#ifndef GENUIS_SONG_LYRIC_UNORDEREDMAP_H
+#define GENUIS_SONG_LYRIC_UNORDEREDMAP_H
+
+#import <string>
+#import <list>
+using namespace std;
+
+
+struct Node {
+    string artist;
+    string title;
+    Node(string &inputTitle, string &inputArtist) {title = inputTitle; artist = inputArtist;}
+};
+
+class UnorderedMap {
+private:
+    list<Node> *array;
+    float loadFactor;
+    int capacity;
+    int size;
+public:
+    UnorderedMap(int inputCapacity);
+    ~UnorderedMap();
+    float getLoadFactor();
+    int hashFunction(string &title, int inputCapacity);
+    void addSong(string title, string artist);
+    Node* searchSong(string title);
+    void rebuild();
+};
+
+
+#endif //GENUIS_SONG_LYRIC_UNORDEREDMAP_H
