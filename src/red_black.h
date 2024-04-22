a52f5c81d4ee0005721245c5ba372eb20769189a
Ethanholtking <127417494+Ethanholtking@users.noreply.github.com>
2024-04-21 23:04:19 -0400

    Fix: fix insertion

--- src/red_black.h
+++ src/red_black.h
@@ -5,7 +5,7 @@
 #include <queue>
 using namespace std;
 
-struct red_black_node
+class red_black_node
 {
 public:
 	// Variables for the node
@@ -26,16 +26,26 @@ public:
 	}
 };
 
-struct red_black_tree
+class red_black_tree
 {
+private:
 	red_black_node* root;
     int size;
+	bool ll;
+	bool rr;
+	bool lr;
+	bool rl;
 
+public:
 	//Constructor
 	red_black_tree()
 	{
 		this->root = nullptr;
         this->size = 0;
+		ll = false;
+		rr = false;
+		lr = false;
+		rl = false;
 	}
 	// Getter
 	red_black_node* get_root()
@@ -61,12 +71,12 @@ struct red_black_tree
 		delete curr;
 	}
 	string to_lower(string title);
-	red_black_node* insert_node(red_black_node* root, string title);
-	void right_rot(red_black_node* root);
-	void left_rot(red_black_node* root);
+	red_black_node* insertHelp(red_black_node* root, string data);
+	void insert(string data);
+	red_black_node* right_rot(red_black_node* root);
+	red_black_node* left_rot(red_black_node* root);
 	red_black_node* get_uncle(red_black_node* root);
 	red_black_node* get_grandparent(red_black_node* root);
-	string balance(red_black_node* root);
 	string search(red_black_node* root, string target);
 	vector<string> in_order_traversal(red_black_node* root, vector<string> &titles);
 	float get_song_titles(red_black_node* root, float percent, string target);
@@ -75,131 +85,132 @@ struct red_black_tree
 	vector<pair<string, float>> most_used_words(map<string, float> freq);
 };
 
+red_black_node* red_black_tree::insertHelp(red_black_node* root, string data) {
+	bool f = false; // Flag to check RED-RED conflict
+
+	if (root == nullptr)
+		return new red_black_node(data);
+	else if (data < root->title) {
+		root->left = insertHelp(root->left, data);
+		root->left->parent = root;
+		if (root != this->root) {
+			if (root->red == true && root->left->red == true)
+				f = true;
+		}
+	}
+	else {
+		root->right = insertHelp(root->right, data);
+		root->right->parent = root;
+		if (root != this->root) {
+			if (root->red == true && root->right->red == true)
+				f = true;
+		}
+	}
+
+	// Perform rotations
+	if (ll == true)
+	{
+		root = left_rot(root);
+		root->red = false;
+		root->left->red = true;
+		ll = false;
+	}
+	else if (rr == true) {
+		root = right_rot(root);
+		root->red = false;
+		root->right->red = true;
+		rr = false;
+	}
+	else if (rl == true) {
+		root->right = right_rot(root->right);
+		root->right->parent = root;
+		root = left_rot(root);
+		root->red = false;
+		root->left->red = true;
+		rl = false;
+	}
+	else if (lr == true) {
+		root->left = left_rot(root->left);
+		root->left->parent = root;
+		root = right_rot(root);
+		root->red = false;
+		root->right->red = true;
+		lr = false;
+	}
+
+	// Handle RED-RED conflicts
+	if (f) {
+		if (root->parent->right == root) {
+			if (root->parent->left == nullptr || root->parent->left->red == false) {
+				if (root->left != nullptr && root->left->red == true)
+					rl = true;
+				else if (root->right != nullptr && root->right->red == true)
+					ll = true;
+			}
+			else {
+				root->parent->left->red = false;
+				root->red = false;
+				if (root->parent != this->root)
+					root->parent->red = true;
+			}
+		}
+		else {
+			if (root->parent->right == nullptr || root->parent->right->red == false) {
+				if (root->left != nullptr && root->left->red == true)
+					rr = true;
+				else if (root->right != nullptr && root->right->red == true)
+					lr = true;
+			}
+			else {
+				root->parent->right->red = false;
+				root->red = false;
+				if (root->parent != this->root)
+					root->parent->red = true;
+			}
+		}
+		f = false;
+	}
+	return root;
+}
+
+void red_black_tree::insert(string data) {
+	if (root == nullptr) {
+		root = new red_black_node(data);
+		root->red = false;
+	}
+	else
+		root = insertHelp(root, data);
+}
+
+red_black_node* red_black_tree::right_rot(red_black_node* node) {
+	red_black_node* x = node->left;
+	red_black_node* y = x->right;
+	x->right = node;
+	node->left = y;
+	node->parent = x;
+	if (y != nullptr)
+		y->parent = node;
+	return x;
+}
+
+red_black_node* red_black_tree::left_rot(red_black_node* node) {
+	red_black_node* x = node->right;
+	red_black_node* y = x->left;
+	x->left = node;
+	node->right = y;
+	node->parent = x;
+	if (y != nullptr)
+		y->parent = node;
+	return x;
+}
+
+
+
 string red_black_tree::to_lower(string title)
 {
 	for (int i = 0; i < title.length(); i++)
 		title[i] = tolower(title[i]);
 	return title;
-}
-
-red_black_node* red_black_tree::insert_node(red_black_node* root, string title)
-{
-	string rot;
-	red_black_node* prev;
-	red_black_node* prev_left;
-	red_black_node* prev_right;
-	title = to_lower(title);
-	// If the tree is empty
-	if (this->root == nullptr)
-	{
-		red_black_node* tree_root = new red_black_node(title);
-		tree_root->red = false;
-        this->root = tree_root;
-		return tree_root;
-	}
-	// inserts a node
-	if (root == nullptr)
-		return new red_black_node(title);
-	// Inserts a node to the left
-	else if (title < root->title)
-	{
-		prev = root->parent;
-		prev_left = root->left;
-		prev_right = root->right;
-		root->left = insert_node(root->left, title);
-		// Updates the left nodes parent
-		if (root->left != nullptr)
-			root->left->parent = root;
-		rot = balance(root->left);
-		// Changes the return node if a rotation occured
-		if (rot != "" || root->parent != prev)
-		{
-			if (rot == "l")
-				return root->left->right;
-			if (rot == "r")
-				return root->right->left;
-			else if (rot == "lr")
-				return root->parent->right->left;
-			else if (rot == "rl")
-				return root->parent->left->right;
-			else if (root->parent != prev)
-				return root->parent;
-		}
-	}
-	// Inserts a node to the right
-	else if (title > root->title)
-	{
-		prev = root->parent;
-		prev_left = root->left;
-		prev_right = root->right;
-		root->right = insert_node(root->right, title);
-		// Updates the right nodes parent
-		if (root->right != nullptr)
-			root->right->parent = root;
-		rot = balance(root->right);
-		// Changes the return node if a rotation occured
-		if (rot != "" || root->parent != prev)
-		{
-			if (rot == "l")
-				return root->left->right;
-			if (rot == "r")
-				return root->right->left;
-			else if (rot == "lr")
-				return root->parent->right->left;
-			else if (rot == "rl")
-				return root->parent->left->right;
-			else if (root->parent != prev)
-				return root->parent;
-		}
-	}
-    size++;
-	return root;
-}
-
-// Right rotation
-void red_black_tree::right_rot(red_black_node* root)
-{
-	red_black_node* saved = root->left->right;
-	root->left->parent = root->parent;
-	// rotation at the root
-	if (root->parent == nullptr)
-		root->left->parent = nullptr;
-	// Updating parents child
-	else if (root->parent->right == root)
-		root->parent->right = root->left;
-	else
-		root->parent->left = root->left;
-	// Updating node pointers
-	root->parent = root->left;
-	root->left->right = root;
-	root->left = saved;
-	// Udpates the saved nodes parent if it is not null
-	if (root->left != nullptr)
-		root->left->parent = root;
-	return;
-}
-
-// Left rotation
-void red_black_tree::left_rot(red_black_node* root)
-{
-	red_black_node* saved = root->right->left;
-	root->right->parent = root->parent;
-	// rotation at the root
-	if (root->parent == nullptr)
-		root->right->parent = nullptr;
-	// Updating parents child
-	else if (root->parent->right == root)
-		root->parent->right = root->right;
-	else
-		root->parent->left = root->right;
-	// Updating node pointers
-	root->parent = root->right;
-	root->right->left = root;
-	root->right = saved;
-	// Udpates the saved nodes parent if it is not null
-	if (root->right != nullptr)
-		root->right->parent = root;
-	return;
 }
 
 red_black_node* red_black_tree::get_uncle(red_black_node* root)
@@ -229,80 +240,6 @@ red_black_node* red_black_tree::get_grandparent(red_black_node* root)
 	return root->parent->parent;
 }
 
-// Balancing
-string red_black_tree::balance(red_black_node* root)
-{
-	string rot = "";
-	// If the root is a null pointer no changes need to be made
-	if (root == nullptr)
-		return rot;
-	// If the root is red make it black
-	if (root->parent == nullptr)
-	{
-		root->red = false;
-		return rot;
-	}
-	// If the parent is not red or the root itself is black no changes needed
-	if (root->parent->red == false || root->red == false)
-		return rot;
-	red_black_node* parent = root->parent;
-	red_black_node* grandparent = get_grandparent(root);
-	red_black_node* uncle = get_uncle(root);
-	// Color fliping
-	if (uncle != nullptr && uncle->red == true)
-	{
-		parent->red = false;
-		uncle->red = false;
-		grandparent->red = true;
-	}
-	else if (grandparent != nullptr)
-	{
-		if (grandparent->left != nullptr)
-		{
-			// Left-right rotation
-			if (root == parent->right && parent == grandparent->left)
-			{
-				left_rot(parent);
-				root = parent;
-				parent = root->parent;
-				rot = "lr";
-			}
-		}
-		else if (grandparent->right != nullptr)
-		{
-			// Right-left rotation
-			if (root == parent->left && parent == grandparent->right)
-			{
-				right_rot(parent);
-				root = parent;
-				parent = root->parent;
-				rot = "rl";
-			}
-		}
-		// Changes rotated node colors
-		parent->red = false;
-		grandparent->red = true;
-		// right rotation
-		if (root == parent->left)
-		{
-			right_rot(grandparent);
-			// If it is not a left right rotation, then it is a right
-			if (rot == "")
-				rot = "r";
-			return rot;
-		}
-		// left rotation
-		else
-		{
-			left_rot(grandparent);
-			// If it is not a right left rotation, then it is a left
-			if (rot == "")
-				rot = "l";
-			return rot;
-		}
-	}
-	return rot;
-}
 string red_black_tree::search(red_black_node* root, string target)
 {
 	if (root == nullptr)
